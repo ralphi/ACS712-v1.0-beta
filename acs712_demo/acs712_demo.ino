b6c551eeee6304f652f80cfd681381384690ab0f
ralphi <ralphi4211@t-online.de>
2016-06-21 20:35:54 +0200

    Add files via upload

---
+++ acs712_demo/acs712_demo.ino
@@ -0,0 +1,20 @@
+#include <ACS712.h>
+ACS712 ACS;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Start");
+
+  uint16_t half_bit = ACS.Set_ADC(10);
+  Serial.print("half_bit: ");
+  Serial.println(half_bit);
+  long vcc = ACS.Get_vcc_analog();
+  Serial.print("vcc: ");
+  Serial.println(vcc);
+}
+void loop() {
+  double current = ACS.Get_current(A1,5);
+  Serial.print("current: ");
+  Serial.println(current);
+  delay(100);
+}
